d1b20e0fe03a96bb5db20fcf909f77306f568216
builtbysilo <cody@builtbysilo.com>
2024-04-09 15:02:15 -0500

    useSerach fix?

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import CustomCursor from 'components/customcursor';
 import Gallery from 'components/gallery';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className="">
-      <CustomCursor />
-      <div className="hidden md:inline">
-        <Gallery scale={5} />
-      </div>
-      <div className="inline md:hidden">
-        <Gallery scale={10} />
-      </div>
+      <Suspense>
+        <CustomCursor />
+        <div className="hidden md:inline">
+          <Gallery scale={5} />
+        </div>
+        <div className="inline md:hidden">
+          <Gallery scale={10} />
+        </div>
+      </Suspense>
     </div>
   );
 }
