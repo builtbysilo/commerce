13e067488c86ce80042b1d37431faaeceaf1ad0c
builtbysilo <cody@builtbysilo.com>
2024-04-09 16:20:56 -0500

    maybe?

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  images: {
-    formats: ['image/avif', 'image/webp'], // Only include supported formats here
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  },
+  // images: {
+  //   formats: ['image/avif', 'image/webp'], // Only include supported formats here
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'cdn.shopify.com',
+  //       pathname: '/s/files/**'
+  //     }
+  //   ]
+  // },
   async redirects() {
     return [
       {
