481d3dbd9d147f499a1d29fd9011f6fb9e446b34
builtbysilo <cody@builtbysilo.com>
2024-04-10 23:01:29 -0500

    Trying to fix image path shit

--- components/three-d-poster.jsx
+++ components/three-d-poster.jsx
@@ -19,7 +19,7 @@ const ThreeDPoster = () => {
       {/* <ambientLight intensity={Math.PI / 2} /> */}
       {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} /> */}
       {/* <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
-      <Poster img="/3DPosterTest.jpg" />
+      <Poster img="/3DPosters/3DPosterTest.jpg" />
       <OrbitControls />
     </Canvas>
   );
